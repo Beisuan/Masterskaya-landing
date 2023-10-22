0d6b439e3a31034c5eac17d5301e4b235fc11739
Beisuan <artemk2003@mail.ru>
2023-10-22 19:16:28 +0300

    feat:add spinner fixed: errors

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,3 +1,16 @@
 (function () {
   'use strict';
 });
+$(document).ready(function() {
+  $('.hamburger').click(function(){
+      $('.hamburger').toggleClass('menu-open');
+      $('.menu').toggleClass('open-menu');
+  });
+});
+window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function() {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 2000);
+}
