ef286a00f65a7f34de63d534cab1727a123fa41d
Beisuan <artemk2003@mail.ru>
2023-11-01 23:23:10 +0300

    feat: prettier format fixed: scrolling in menu mobile fixed: main.js no jquery exclude owl fixed: carousel slider background

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
   'use strict';
   const loaderTimer = (time) =>
-  new Promise((resolve) => {
-    const timerId = window.setTimeout(() => {
-      resolve(timerId);
-    }, time)
-  });
+    new Promise((resolve) => {
+      const timerId = window.setTimeout(() => {
+        resolve(timerId);
+      }, time);
+    });
   loaderTimer(500).then((timerId) => {
     window.clearTimeout(timerId);
-    $('body').addClass('loaded');
-    $('body').removeClass('loaded_hiding');
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
   });
 });
-  $(document).ready(function(){
-    const slider = $("#slider").owlCarousel({
-      items: 1,
-      nav:true,
-      loop:true
-      
-    });
-    document.querySelector('.hamburger').addEventListener('click', function() {
-      document.querySelector('.menu').classList.toggle('open');
-        });
+$(document).ready(function () {
+  const slider = $('#slider').owlCarousel({
+    items: 1,
+    nav: true,
+    loop: true,
   });
+  document.querySelector('.hamburger').addEventListener('click', function () {
+    document.querySelector('.menu').classList.toggle('open');
+    document.querySelector('body').classList.toggle('no-scroll');
+  });
+});
