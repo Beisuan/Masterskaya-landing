2a6d589b1269fda32fc415960b1bfec699e014fb
Beisuan <artemk2003@mail.ru>
2023-11-29 17:40:54 +0300

    finally fixed

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,29 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
+(function () {
   'use strict';
-  const loaderTimer = (time) =>
-    new Promise((resolve) => {
-      const timerId = window.setTimeout(() => {
-        resolve(timerId);
-      }, time);
-    });
-  loaderTimer(500).then((timerId) => {
-    window.clearTimeout(timerId);
-    document.body.classList.add('loaded');
-    document.body.classList.remove('loaded_hiding');
-  });
+
 });
-$(document).ready(function () {
-  const boxes = Array.from(document.querySelectorAll(".box")); 
-  const slider = $('#slider').owlCarousel({
-    items: 1,
-    nav: true,
-    loop: true,
-  });
-  document.querySelector('.hamburger').addEventListener('click', function () {
-    document.querySelector('.menu').classList.toggle('open');
-    document.querySelector('body').classList.toggle('no-scroll');
-  });
-  boxes.forEach((box) => {
+const boxes = Array.from(document.querySelectorAll(".box")); 
+
+boxes.forEach((box) => {
   box.addEventListener("click", boxHandler); 
 });
 
@@ -32,7 +13,7 @@ function boxHandler(e) {
   let currentBox = e.target.closest(".box");
   let currentContent = e.target.nextElementSibling;
 
-  
+
   currentBox.classList.toggle("active"); 
   if (currentBox.classList.contains("active")) {
     currentContent.style.maxHeight = currentContent.scrollHeight + "px"; 
@@ -40,6 +21,3 @@ function boxHandler(e) {
     currentContent.style.maxHeight = 0; 
   }
   }
-});
-
-
