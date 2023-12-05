140365b79bcf73b2a82130dbc8d4829380d6e974
Beisuan <artemk2003@mail.ru>
2023-12-05 18:40:08 +0300

    feat: working boxes

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,23 +1,28 @@
 (function () {
   'use strict';
-
 });
-const boxes = Array.from(document.querySelectorAll(".box")); 
+const boxes = Array.from(document.querySelectorAll('.box'));
 
 boxes.forEach((box) => {
-  box.addEventListener("click", boxHandler); 
+  box.addEventListener('click', boxHandler);
 });
 
 function boxHandler(e) {
-  e.preventDefault(); 
-  let currentBox = e.target.closest(".box");
+  e.preventDefault();
+  let currentBox = e.target.closest('.box');
   let currentContent = e.target.nextElementSibling;
 
+  boxes.forEach((elem) => {
+    if (elem.classList.contains('active') && elem != currentBox) {
+      elem.classList.toggle('active');
+      elem.querySelector('div.content').style.maxHeight = 0;
+    }
+  });
 
-  currentBox.classList.toggle("active"); 
-  if (currentBox.classList.contains("active")) {
-    currentContent.style.maxHeight = currentContent.scrollHeight + "px"; 
+  currentBox.classList.toggle('active');
+  if (currentBox.classList.contains('active')) {
+    currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
   } else {
-    currentContent.style.maxHeight = 0; 
+    currentContent.style.maxHeight = 0;
   }
-  }
+}
